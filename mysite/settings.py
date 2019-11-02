d903b6749a1e024a46223a879108bdd03a2ae9f8
Your Name <you@example.com>
2019-11-02 14:58:23 -0500

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#wox%rn_uk*dv0@8g32yq73by)+!rhvj2i7rmxj%s^oa&f!u)*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
